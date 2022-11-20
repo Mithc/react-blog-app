34281d4ea14dbbb0b231d5eec7060107f297735a
dfedorov <dfedorov@teh-lab.ru>
2022-11-20 16:32:35 +0100

    Moved fetching logick to custom hook

---
+++ src/customHooks/useFetch.ts
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import { Post } from "../interfaces/post";
+
+
+const useFetch = (url: string) => {
+    const [data, setData] = useState<Post[]>([]);
+    const [isPending, setIsPending] = useState<boolean>(true)
+    const [error, setError] = useState<string | null>(null)
+
+    useEffect(() => {
+        setTimeout(() => {
+            fetch(url)
+                .then(res => {
+                    if (!res.ok) {
+                    }
+                    res.json().then(data => {
+                        setData(data as Post[])
+                        setIsPending(false)
+                        setError(null)
+                    })
+                })
+                .catch(e => {
+                    setError(e)
+                    setIsPending(false)
+                });
+        }, 500)
+    }, [url])
+
+    return { data, isPending, error }
+}
+
+export default useFetch;
