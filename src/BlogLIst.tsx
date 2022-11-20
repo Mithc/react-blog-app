72fe6e8bd4f1bc1e390a0f04a40e679f26d93c17
dfedorov <dfedorov@teh-lab.ru>
2022-11-20 15:57:49 +0100

    added db.json for mock backend

--- src/BlogLIst.tsx
+++ src/BlogLIst.tsx
@@ -2,20 +2,22 @@ import React from "react";
 import { Post } from "./interfaces/post";
 
 interface BlogListProps {
-    blogs: Post[]
+    blogs: Post[],
+    handleDelete: (id: number) => void
 }
 
-const BlogList: React.FC<BlogListProps> = ({blogs}) => {
+const BlogList: React.FC<BlogListProps> = ({ blogs , handleDelete}) => {
     return (
         <div className="blog-list">
             {
-            blogs.map((blodPost: Post) =>
-                <div className="blog-preview" key={blodPost.id}>
-                    <h2 className="blog-title">{blodPost.title}</h2>
-                    <p className="blog-author">Written by {blodPost.author}</p>
-                    <p className="postDate">{blodPost.date.toString().substring(0, 15)}</p>
-                </div>)
-        }
+                blogs.map((blodPost: Post) =>
+                    <div className="blog-preview" key={blodPost.id}>
+                        <h2 className="blog-title">{blodPost.title}</h2>
+                        <p className="blog-author">Written by {blodPost.author}</p>
+                        <p className="postDate">{blodPost.date.toString().substring(0, 15)}</p>
+                        <button onClick={() => handleDelete(blodPost.id)}>Delete</button>
+                    </div>)
+            }
         </div>
     );
 
