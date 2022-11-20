37fd30e99bab8696d494a3cedbeeb54a5599c1a2
dfedorov <dfedorov@teh-lab.ru>
2022-11-20 16:32:13 +0100

    Moced fetching logick to custom hook

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,42 +1,20 @@
-import { useEffect, useState } from "react";
 import BlogList from "./BlogLIst";
+import useFetch from "./customHooks/useFetch";
 import { Post } from "./interfaces/post";
 
 const Home: React.FC = () => {
-    const [blogs, setBlogs] = useState<Post[]>([]);
-    const [isPending, setIsPending] = useState<boolean>(true)
-    const [error, setError] = useState<string | null>(null)
+    const { data: blogs, isPending, error }: { data: Post[], isPending: boolean, error: string | null } = useFetch('http://localhost:4200/blogs')
+    // const handleDelete = (id: number) => setBlogs(blogs.filter(blog => blog.id !== id))
 
-    useEffect(() => {
-        setTimeout(() => {
-            fetch('http://localhost:4200/blogs')
-                .then(res => {
-                    if (!res.ok) {
-                        throw Error('Data couldn\'t be fatched')
-                    }
-                    res.json().then(data => {
-                        setBlogs(data as Post[])
-                        setIsPending(false)
-                    })
-                })
-                .catch(e => {
-                    setError(e)
-                    setIsPending(false)
-                });
-        }, 500)
-    }, [])
-
-    const handleDelete = (id: number) => setBlogs(blogs.filter(blog => blog.id !== id))
-
-    useEffect(() => {
-        console.log('on delete');
-    }, [handleDelete])
+    // useEffect(() => {
+    //     console.log('on delete');
+    // }, [handleDelete])
 
 
     return (<div className="home">
         {error && <div className="error"> {error}</div>}
         {isPending && <div className="loading-massage">LOADING.... It would be cool to add loader here :\</div>}
-        {!isPending && <BlogList blogs={blogs} handleDelete={handleDelete} />}
+        {!isPending && <BlogList blogs={blogs} handleDelete={() => null} />}
     </div>);
 }
 
